d6144c817688a793729ed78c008f7c0dcfa76ce3
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-06-10 10:06:49 -0700

    No real changes

--- starter-code/app.js
+++ starter-code/app.js
@@ -119,11 +119,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // eslint-disable-next-line no-undef
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
